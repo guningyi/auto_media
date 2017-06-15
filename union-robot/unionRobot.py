44a43678b4b6f21bd5ddabc517bdc8ef50b4429b
guningyi <ningyi.gu@ericsson.com>
2017-06-15 17:27:30 +0800

    fix the error

--- union-robot/unionRobot.py
+++ union-robot/unionRobot.py
@@ -1,2 +1,16 @@
 import win32gui
+import win32api
+import win32con
+
+
+def mouse_click(pos):
+    win32api.SetCursorPos(pos)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN | win32con.MOUSEEVENTF_RIGHTUP,0,0,0,0)
+
+if __name__ == "__main__":
+    dlg = win32gui.FindWindow(None, 'Internet Explorer')
+    win32gui.ShowWindow(dlg, win32con.SW_RESTORE)
+    mouse_click((1500, 500))
+
+
 

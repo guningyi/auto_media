886db2e9bb001166728eb85df00f677e7e535b97
guningyi <ningyigu@gmail.com>
2017-07-18 23:01:40 +0800

    add the test function for firefox

--- union-robot/test_webdriver_proxy_for_firefox.py
+++ union-robot/test_webdriver_proxy_for_firefox.py
@@ -11,8 +11,8 @@ from selenium.webdriver.common.proxy import ProxyType
 
 if __name__ == '__main__':
 
-    ieDriver = "C:\Program Files\Internet Explorer\IEDriverServer.exe"
-    os.environ["webdriver.ie.driver"] = ieDriver
+    #fireFoxDriver = "C:\\Users\\100plus\AppData\Local\Programs\Python\Python36\geckodriver.exe"
+    #os.environ["webdriver.firefox.driver"] = fireFoxDriver
 
     #设置代理 111.155.116.195 8123
     proxy = Proxy(
@@ -22,14 +22,8 @@ if __name__ == '__main__':
     }
     )
 
-
-    browser =  webdriver.Ie(ieDriver)
-
+    browser =  webdriver.Firefox(proxy=proxy)
     browser.get("https://www.so.com/s?src=lm&ls=sm1515585&q=%E7%89%A9%E6%B5%81%E5%BF%AB%E9%80%92&lmsid=92de997c60a756f8&lm_extend=ctype:7")
-    #browser.switch_to.frame('e_idea_frame_1')  # 需先跳转到iframe框架
-    #browser.find_element_by_id("e_idea_pp").send_keys("selenium")
-    browser.find_elements_by_id('e_idea_pp')
-    time.sleep(2)
 
     #使用css来精确定位href
     inputs = browser.find_elements_by_css_selector("#e_idea_pp li h3 a")
